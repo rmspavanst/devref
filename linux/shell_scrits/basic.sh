6824fef0e0168276d927ce38783fd846a5aae95a
pavan <pavanrms.st@gmail.com>
2022-08-09 16:30:30 +0800

    yy

---
+++ linux/shell_scrits/basic.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# Author:
+# Date:
+# Description: This script will run basic admin task
+
+echo
+echo This script will run basic administrative cmds
+echo
+top
+echo
+echo
+df -h
+echo
+free -h
+echo
+uptime
+echo4
+iostat
